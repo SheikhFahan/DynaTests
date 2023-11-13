510e529acfa20d1789b23c1ee7c1802f92febcd5
e-lec-tron <business.fahan@gmail.com>
2023-11-13 12:00:03 +0530

    db optimizations

--- backend/tests/admin.py
+++ backend/tests/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Test, Category, Choice, Question
+from .models import Test, Category, EasyQuestion, MediumQuestion, HardQuestion, ChoiceForEasyQ ,ChoiceForMediumQ, ChoiceForHardQ
 
 admin.site.register(Test)
 admin.site.register(Category)
-admin.site.register(Choice)
-admin.site.register(Question)
+admin.site.register(EasyQuestion)
+admin.site.register(MediumQuestion)
+admin.site.register(HardQuestion)
+admin.site.register(ChoiceForEasyQ)
+admin.site.register(ChoiceForMediumQ)
+admin.site.register(ChoiceForHardQ)
+
--- backend/tests/models.py
+++ backend/tests/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 import pandas as pd
 
-# Create your models here.
+# for dynamic foreign keys
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 class Category(models.Model):
     name = models.CharField(max_length=15)
@@ -12,14 +15,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Test(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    ]
     title = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=10,choices= DIFFICULTY_CHOICES)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     test_file = models.FileField(upload_to='media/test_files/')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+    updated_at = models.DateTimeField(auto_now=True)   
 
     def __str__(self):
         return self.title
@@ -38,30 +47,81 @@ class Test(models.Model):
         for index, row in df.iterrows():
             # extract question text, choices and correct answer from the row
             question_text = row['Question']
-            choice1 = row['A']
-            choice2 = row['B']
-            choice3 = row['C']
-            choice4 = row['D']
+            choice1, choice2, choice3, choice4 = row['A'], row['B'], row['C'], row['D']
             correct_answer = row['Answer']
 
-            # create the question object
-            question = Question.objects.get_or_create(quiz=self, text=question_text)
+            
+            # for dynamic foreign key(not usefull for now)
+                # getting the foreign key reference 
+                # content_type = ContentType.objects.get_for_model(self)
+                # create the question object
+                # question = Question.objects.get_or_create(content_type = content_type, object_id = self.id, text=question_text)
 
-            # create choices objects
-            choice_1 = Choice.objects.get_or_create(question=question[0], text=choice1, is_correct=correct_answer == 'A')
-            choice_2 = Choice.objects.get_or_create(question=question[0], text=choice2, is_correct=correct_answer == 'B')
-            choice_3 = Choice.objects.get_or_create(question=question[0], text=choice3, is_correct=correct_answer == 'C')
-            choice_4 = Choice.objects.get_or_create(question=question[0], text=choice4, is_correct=correct_answer == 'D')
+            if(self.difficulty == 'easy'):
+                question = EasyQuestion.objects.get_or_create(test=self, text=question_text)
+                choice_1 = ChoiceForEasyQ.objects.get_or_create(question=question[0], text=choice1, is_correct=correct_answer == 'A')
+                choice_2 = ChoiceForEasyQ.objects.get_or_create(question=question[0], text=choice2, is_correct=correct_answer == 'B')
+                choice_3 = ChoiceForEasyQ.objects.get_or_create(question=question[0], text=choice3, is_correct=correct_answer == 'C')
+                choice_4 = ChoiceForEasyQ.objects.get_or_create(question=question[0], text=choice4, is_correct=correct_answer == 'D')
+                
+            elif(self.difficulty == 'medium'):
+                question = MediumQuestion.objects.get_or_create(test=self, text=question_text)
+                choice_1 = ChoiceForMediumQ.objects.get_or_create(question=question[0], text=choice1, is_correct=correct_answer == 'A')
+                choice_2 = ChoiceForMediumQ.objects.get_or_create(question=question[0], text=choice2, is_correct=correct_answer == 'B')
+                choice_3 = ChoiceForMediumQ.objects.get_or_create(question=question[0], text=choice3, is_correct=correct_answer == 'C')
+                choice_4 = ChoiceForMediumQ.objects.get_or_create(question=question[0], text=choice4, is_correct=correct_answer == 'D')
 
-class Question(models.Model):
-    quiz = models.ForeignKey(Test, on_delete=models.CASCADE)
+            elif(self.difficulty =='hard'):
+                question = HardQuestion.objects.get_or_create(test=self, text=question_text)
+                choice_1 = ChoiceForHardQ.objects.get_or_create(question=question[0], text=choice1, is_correct=correct_answer == 'A')
+                choice_2 = ChoiceForHardQ.objects.get_or_create(question=question[0], text=choice2, is_correct=correct_answer == 'B')
+                choice_3 = ChoiceForHardQ.objects.get_or_create(question=question[0], text=choice3, is_correct=correct_answer == 'C')
+                choice_4 = ChoiceForHardQ.objects.get_or_create(question=question[0], text=choice4, is_correct=correct_answer == 'D')
+
+class EasyQuestion(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    # generic-foreign-key
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # object_id = models.PositiveIntegerField()
+    # test = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.text[:50]
+    
+class MediumQuestion(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     text = models.TextField()
 
     def __str__(self):
         return self.text[:50]
     
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+class HardQuestion(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+    
+class ChoiceForEasyQ(models.Model):
+    question = models.ForeignKey(EasyQuestion, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.question.text[:50]}, {self.text[:20]}"
+    
+class ChoiceForMediumQ(models.Model):
+    question = models.ForeignKey(MediumQuestion, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.question.text[:50]}, {self.text[:20]}"
+    
+class ChoiceForHardQ(models.Model):
+    question = models.ForeignKey(HardQuestion, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
 

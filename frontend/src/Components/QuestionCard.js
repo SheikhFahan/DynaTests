444a37a1e062fef54e644471312a7ce2c35a67ba
e-lec-tron <business.fahan@gmail.com>
2023-11-18 17:51:09 +0530

    successfull  receive  of test questions

--- frontend/src/Components/QuestionCard.js
+++ frontend/src/Components/QuestionCard.js
@@ -1,60 +1,59 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react'
+
+import axios from 'axios'
 
 const QuestionCard = () => {
-    const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
+
+
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
-    const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+    const [loading , setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    let [questions, setQuestions] = useState([]);
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
+    const easy_question_array = questions.easy_questions;
+    
+    // console.log(easy_question_array);
+    console.log(questions);
+
+    
+    useEffect(() => {
+        axios
+        .get("http://127.0.0.1:8000/api/tests/1/get_test/")
+        .then((response) => {
+            setQuestions(response.data)
+            setLoading(false)
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+            setError('Error fetching the data please try again');
+            setLoading(false);
+        });
+    }, []);
+      
+    // const handleAnswerOptionClick = (isCorrect) => {
+	// 	if (isCorrect) {
+	// 		setScore(score + 1);
+	// 	}
+
+	// 	const nextQuestion = currentQuestion + 1;
+	// 	if (nextQuestion < questions.length) {
+	// 		setCurrentQuestion(nextQuestion);
+	// 	} else {
+	// 		setShowScore(true);
+	// 	}
+	// }
+    //    {/* {questions[currentQuestion].answerOptions.map((answerOption) => ( */}
+     //       // <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+     //   {/* ))} */}
+     if(loading) {
+        return <div>Loading...</div>
+     }
+     if (error) {
+        return <div>{error}</div>;
+      }
   return (
     <div className='app'>
 			{showScore ? (
@@ -62,22 +61,31 @@ const QuestionCard = () => {
 					You scored {score} out of {questions.length}
 				</div>
 			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
+				<>      {easy_question_array.map((question) => (
+                        <div>
+                        <div className='question-section' key={questions.id}>
+                            <div className='question-count'>
+                                <span>Question {currentQuestion + 1}</span>/{questions.length}
+                            </div>
+                            <div className='question-text'>question{questions.title}</div>
+                        </div>
+                        
+
+                            <div className='answer-section'>
+                                    <button>{question.text}</button>
+                                    {question.choices.map((choice) =>(
+                                      <button>{choice.text}</button>
+                                    ))} 
+
+                            </div>
+                         
+                    
+                    </div>
+                    ))} 
 				</>
-			)}
+            )}<br/>
 		</div>
 	);
-}
+};
 
 export default QuestionCard

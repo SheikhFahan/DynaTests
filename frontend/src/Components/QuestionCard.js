8a28bad0c3cda55967177c216b1d4f9c70aa44ad
e-lec-tron <business.fahan@gmail.com>
2023-11-24 08:10:51 +0530

    added answer selection function and saperated the questions card

--- frontend/src/Components/QuestionCard.js
+++ frontend/src/Components/QuestionCard.js
@@ -3,21 +3,16 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const QuestionCard = () => {
-
-
-
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
     const [loading , setLoading] = useState(true);
     const [error, setError] = useState(null);
-    let [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState({});
+    const [questions, setQuestions] = useState([]);
 
     const easy_question_array = questions.easy_questions;
     
-    // console.log(easy_question_array);
-    console.log(questions);
-
     
     useEffect(() => {
         axios
@@ -25,29 +20,24 @@ const QuestionCard = () => {
         .then((response) => {
             setQuestions(response.data)
             setLoading(false)
+            // console.log(JSON.stringify(questions))
+            // console.log("coming here ")
+
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
             setError('Error fetching the data please try again');
-            setLoading(false);
+            setLoading(true);
         });
     }, []);
       
-    // const handleAnswerOptionClick = (isCorrect) => {
-	// 	if (isCorrect) {
-	// 		setScore(score + 1);
-	// 	}
-
-	// 	const nextQuestion = currentQuestion + 1;
-	// 	if (nextQuestion < questions.length) {
-	// 		setCurrentQuestion(nextQuestion);
-	// 	} else {
-	// 		setShowScore(true);
-	// 	}
-	// }
-    //    {/* {questions[currentQuestion].answerOptions.map((answerOption) => ( */}
-     //       // <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-     //   {/* ))} */}
+    function handleAnswers(questionId, choiceId) {
+        
+        setAnswers((prevAnswers) => ({
+            ...prevAnswers,
+            [questionId] : choiceId,
+        }))
+    }
      if(loading) {
         return <div>Loading...</div>
      }
@@ -55,36 +45,36 @@ const QuestionCard = () => {
         return <div>{error}</div>;
       }
   return (
-    <div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>      {easy_question_array.map((question) => (
-                        <div>
-                        <div className='question-section' key={questions.id}>
-                            <div className='question-count'>
-                                <span>Question {currentQuestion + 1}</span>/{questions.length}
-                            </div>
-                            <div className='question-text'>question{questions.title}</div>
-                        </div>
-                        
+    showScore ? (
+        <div className='score-section'>
+            You scored {score} out of 
+        </div>
+    ) : (
+        <>
+        <h1>{questions.title}</h1>
+        {easy_question_array.map((question) => (
+        <div className='app' key={question.id}>            
+            <div className='question-section' >
+                <div className='question-count'>
+                    <span>Question {currentQuestion + 1} </span>
+                    <br></br>
+                </div>
+                <div className='question-text'>question {question.text}</div>
+            </div>
+            <div className='answer-section'>
+                {question.choices.map((choice) =>(
+                    <button 
+                        key={choice.id}
+                        onClick={()=> handleAnswers(question.pk, choice.pk)}
+                        >{choice.text}</button>
 
-                            <div className='answer-section'>
-                                    <button>{question.text}</button>
-                                    {question.choices.map((choice) =>(
-                                      <button>{choice.text}</button>
-                                    ))} 
-
-                            </div>
-                         
-                    
-                    </div>
-                    ))} 
-				</>
-            )}<br/>
+                ))} 
+            </div>             			
 		</div>
+        ))}
+        </>
+        )
+    
 	);
 };
 

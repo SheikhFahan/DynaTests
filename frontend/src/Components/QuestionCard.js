81efc1ac895f220a5506b505ec7822d7b8e2c75a
e-lec-tron <business.fahan@gmail.com>
2023-11-27 11:26:14 +0530

    sending choices to backend --final

--- frontend/src/Components/QuestionCard.js
+++ frontend/src/Components/QuestionCard.js
@@ -8,10 +8,25 @@ const QuestionCard = () => {
 	const [score, setScore] = useState(0);
     const [loading , setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [answers, setAnswers] = useState([]);
+    const [answersForEasy, setAnswersForEasy] = useState({
+        easy: [],
+    });
+    const [answersForMedium, setAnswersForMedium] = useState({
+        medium: [],
+    });
+    const [answersForHard, setAnswersForHard] = useState({
+        hard: [],
+    });
+    const allAnswers = {
+        easy: [...answersForEasy.easy],
+        medium: [...answersForMedium.medium],
+        hard: [...answersForHard.hard],
+      };
     const [questions, setQuestions] = useState([]);
 
     const easy_question_array = questions.easy_questions;
+    const medium_question_array = questions.medium_questions; 
+    const difficult_question_array = questions.difficult_questions;    
     
     
     useEffect(() => {
@@ -30,19 +45,61 @@ const QuestionCard = () => {
             setLoading(true);
         });
     }, []);
-      
-    function handleAnswers(difficulty, choiceId) {
-        const newAnswer = {
-            difficulty: difficulty,
-            answer_id: choiceId,
-          };
-        setAnswers((prevAnswers) => [...prevAnswers, newAnswer]);
-        
-    }
+           
+   const handleEasyAnswers = (questionId, choiceId) => {
+        setAnswersForEasy((prevAnswers) => {
+            const existingAnswerIndex = prevAnswers['easy'].findIndex(answer => answer.question_id === questionId);
+            if(existingAnswerIndex !== -1){
+                const updatedAnswers = [...prevAnswers['easy']];
+                updatedAnswers[existingAnswerIndex] = {
+                    question_id: questionId,
+                    answer_id: choiceId
+                };
+
+                    return { ...prevAnswers, easy: updatedAnswers };; 
+            } else {
+                return { ...prevAnswers, easy: [...prevAnswers['easy'], { question_id: questionId, answer_id: choiceId }] };
+            }
+        });
+      };
+      const handleMediumAnswers = (questionId, choiceId) => {
+        setAnswersForMedium((prevAnswers) => {
+            const existingAnswerIndex = prevAnswers['medium'].findIndex(answer => answer.question_id === questionId);
+            if(existingAnswerIndex !== -1){
+                const updatedAnswers = [...prevAnswers['medium']];
+                updatedAnswers[existingAnswerIndex] = {
+                    question_id: questionId,
+                    answer_id: choiceId
+                };
+
+                    return { ...prevAnswers, medium: updatedAnswers };; 
+            } else {
+
+                return { ...prevAnswers, medium: [...prevAnswers['medium'], { question_id: questionId, answer_id: choiceId }] };
+            }
+        });
+      };
+      const handleHardAnswers = (questionId, choiceId) => {
+        setAnswersForHard((prevAnswers) => {
+            const existingAnswerIndex = prevAnswers['hard'].findIndex(answer => answer.question_id === questionId);
+            if(existingAnswerIndex !== -1){
+                const updatedAnswers = [...prevAnswers['hard']];
+                updatedAnswers[existingAnswerIndex] = {
+                    question_id: questionId,
+                    answer_id: choiceId
+                };
+
+                    return { ...prevAnswers, hard: updatedAnswers };; 
+            } else {
+
+                return { ...prevAnswers, hard: [...prevAnswers['hard'], { question_id: questionId, answer_id: choiceId }] };
+            }
+        });
+      };
 
     const handleSubmitAnswers = async (e) => {
         e.preventDefault();
-        const data = answers;
+        const data = allAnswers;
         console.log(data);
         try{
             const response = await axios.post(
@@ -87,7 +144,48 @@ const QuestionCard = () => {
                 {question.choices.map((choice) =>(
                     <button 
                         key={choice.id}
-                        onClick={()=> handleAnswers(choice.difficulty, choice.pk)}
+                        onClick={()=> handleEasyAnswers(question.pk, choice.pk)}
+                        >{choice.text}</button>
+
+                ))} 
+            </div>             			
+		</div>
+        ))}
+        
+        {easy_question_array.map((question) => (
+        <div className='app' key={question.id}>            
+            <div className='question-section' >
+                <div className='question-count'>
+                    <span>Question {currentQuestion + 1} </span>
+                    <br></br>
+                </div>
+                <div className='question-text'>question {question.text}</div>
+            </div>
+            <div className='answer-section'>
+                {question.choices.map((choice) =>(
+                    <button 
+                        key={choice.id}
+                        onClick={()=> handleMediumAnswers(question.pk, choice.pk)}
+                        >{choice.text}</button>
+
+                ))} 
+            </div>             			
+		</div>
+        ))}
+        {easy_question_array.map((question) => (
+        <div className='app' key={question.id}>            
+            <div className='question-section' >
+                <div className='question-count'>
+                    <span>Question {currentQuestion + 1} </span>
+                    <br></br>
+                </div>
+                <div className='question-text'>question {question.text}</div>
+            </div>
+            <div className='answer-section'>
+                {question.choices.map((choice) =>(
+                    <button 
+                        key={choice.id}
+                        onClick={()=> handleHardAnswers(question.pk, choice.pk)}
                         >{choice.text}</button>
 
                 ))} 

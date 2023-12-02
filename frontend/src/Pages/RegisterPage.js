daf456c1d6d6bfd52555bbccd00757f60adc0972
e-lec-tron <business.fahan@gmail.com>
2023-12-02 07:44:46 +0530

    for user registration

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -5,66 +5,83 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const RegisterPage = () => {
-    let [email, setEmail] = useState("");
+  let [userData, setUserData] = useState({
+    email: "",
+    username: "",
+    password1: "",
+    password2: "",
+  });
 
-    let [passwords, setPasswords] = useState({
-      password1: "",
-      password2: "",
-    });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setPasswords((prevPasswords) => ({
-          ...prevPasswords,
-          [name]: value,
-        }));
-      };
-    
-      const handleEmail = (e) => {
-        setEmail(e.target.value);
-      };
 
-    let handleSubmit = async (e) => {
-        if(passwords.password11 !== passwords.password2){
-            return alert("passwords don't match")
+  let handleSubmit = async (e) => {
+    if (userData.password1 !== userData.password2) {
+      console.log(userData.password1, userData.password2)
+      return alert("passwords don't match");
+    }
+    const formData = new FormData();
+    formData.append("email", userData.email);
+    formData.append("password", userData.password1);
+    formData.append("username", userData.username)
+
+    try {
+      const response = axios.post(
+        "//127.0.0.1:8000/api/create_user/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        const formData = new FormData();
-        formData.append("email", email);
-        formData.append("passwords.password1", passwords.password1)
+      );
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          name="Email"
+          onChange={handleInputChange}
+        />
+        <Form.Text className="text-muted">
+          consider your account hacked.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Control type="text" placeholder="Username" name="username" onChange={handleInputChange}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password1"
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control
+          type="password"
+          placeholder="Password Again"
+          name="password2"
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
-        try {
-            const response = axios.post(
-                "//127.0.0.1:8000/api/create_user/", formData, {
-                    headers :{
-                        "Content-Type" : "application/json",
-                    }
-                }
-            )
-        } catch(error) {
-            alert(error);
-        }
-
-    };
-    return (
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Email" name = 'Email' onChange={handleEmail}/>
-          <Form.Text className="text-muted">
-            consider your account hacked.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" name='password1' onChange={handleInputChange}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password Again" name='password2' onChange={handleInputChange} />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    );
-}
-
-export default RegisterPage
+export default RegisterPage;
